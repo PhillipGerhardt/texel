82f0ba7ded63bf6ba5abce550529c449c8056f04
Phillip Gerhardt <phillip@sedna.de>
2024-02-03 10:40:17 +0100

    Animations of properties are stopped when setting a value.

--- Texel/Texel/Layer.swift
+++ Texel/Texel/Layer.swift
@@ -14,24 +14,48 @@ import Combine
 class Layer {
 
     var sublayers = [Layer]()
+
     /// Clip content and childlayers to this layer size
     var clip: Bool = false
+
     /// Draw the layer
     var draw: Bool = true
+
     /// Position of the layer
-    @Published var position = simd_float2.zero
+    @Published var position = simd_float2.zero {
+        didSet { _position = Published<simd_float2>(wrappedValue: position) }
+    }
+
     /// Size of the layer
-    @Published var size = simd_float2.one
+    @Published var size = simd_float2.one {
+        didSet { _size = Published<simd_float2>(wrappedValue: size) }
+    }
+
     /// The orientation of the layer. Used to rotate the layer when the user clicks them with the mouse
-    @Published var orientation = simd_quatf(angle: 0, axis: simd_float3(0,0,1))
+    @Published var orientation = simd_quatf(angle: 0, axis: simd_float3(0,0,1)) {
+        didSet { _orientation = Published<simd_quatf>(wrappedValue: orientation) }
+    }
+
     /// Rotation about z axis. Exported to JS
-    @Published var rotation: Float = 0
+    @Published var rotation: Float = 0 {
+        didSet { _rotation = Published<Float>(wrappedValue: rotation) }
+    }
+
     /// The pivot or anchorpoint. Rotation and position is calculated from that point.
-    @Published var pivot = simd_float2(0.5, 0.5)
+    @Published var pivot = simd_float2(0.5, 0.5) {
+        didSet { _pivot = Published<simd_float2>(wrappedValue: pivot) }
+    }
+
     /// Color used to draw the layer. Default: teal
-    @Published var color = simd_float4(0.0, 0.5, 0.5, 1.0)
+    @Published var color = simd_float4(0.0, 0.5, 0.5, 1.0) {
+        didSet { _color = Published<simd_float4>(wrappedValue: color) }
+    }
+
     /// Color that is multiplied with the color of the content's textures pixels. Can be used to fade the content in and out
-    @Published var contentColor = simd_float4.one
+    @Published var contentColor = simd_float4.one {
+        didSet { _contentColor = Published<simd_float4>(wrappedValue: contentColor) }
+    }
+
     /// Control the volume of the content. Fade content in and out
     @Published var contentVolume: Float = 1
     var content: Content? {
@@ -47,10 +71,13 @@ class Layer {
         }
     }
     var contentVolumeToken: AnyCancellable?
+
     /// Control how the content is laid out in the layer
     var contentScaling: ScaleMode = .fit
+
     /// Control the horizontal alignment of the content in the layer
     var contentHorizontalAlignment: HorizontalAlignment = .center
+
     /// Control the vertical alignment of the content in the layer
     var contentVerticalAlignment: VerticalAlignment = .center
 
